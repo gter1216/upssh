b4e25a37e828159a1fc149ada855813842ac9292
xu xiao <xuxiao1216@126.com>
2019-12-27 16:56:23 +0800

    Create upssh.h

---
+++ upssh.h
@@ -0,0 +1,12 @@
+/*
+ * Author: Xu Xiao
+ * Copyright (c) 2019 Xu Xiao, China
+ *                    All rights reserved
+ */
+ 
+/* Default port number. */
+
+#define SSH_DEFAULT_PORT	1216
+
+#define MAX_LINE 2048
+ 
